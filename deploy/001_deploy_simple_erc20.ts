deccb70454a692bbb8b58c90e3dc2d806c9aaf60
xfu <angerpeanut@gmail.com>
2023-11-30 20:03:49 +0800

    feat: write deployed addresses to file.

--- deploy/001_deploy_simple_erc20.ts
+++ deploy/001_deploy_simple_erc20.ts
@@ -1,19 +1,27 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction} from 'hardhat-deploy/types';
-import {parseEther} from 'ethers';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { parseEther } from 'ethers';
+import { storeAddressList } from "../utils/contractAddress";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-	const {deployments, getNamedAccounts} = hre;
-	const {deploy} = deployments;
+	const { deployments, getNamedAccounts } = hre;
+	const { deploy } = deployments;
 
-	const {deployer, simpleERC20Beneficiary} = await getNamedAccounts();
+	const { deployer, simpleERC20Beneficiary } = await getNamedAccounts();
 
-	await deploy('SimpleERC20', {
+	const SimpleERC20 = await deploy('SimpleERC20', {
 		from: deployer,
-		args: [simpleERC20Beneficiary, parseEther('1000000000')],
+		args: [],
 		log: true,
 		autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
 	});
+
+	let addressObj = {
+		// Contarct deployed local datetime, UTC+8
+		DeployedDatetime: new Date().toLocaleString("zh-CN"),
+		SimpleERC20: SimpleERC20.address,
+	}
+	storeAddressList(addressObj, hre.network.name)
 };
 export default func;
 func.tags = ['SimpleERC20'];
